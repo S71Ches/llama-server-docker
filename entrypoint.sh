927fa79bed05f0c0508ae6b1fcc02dda3d1e2a14
S71Ches <masyacipa124@gmail.com>
2025-05-19 11:46:59 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,6 @@ WORKERS="${WORKERS:-1}"
 echo "[entrypoint] Старт cloudflared Quick Tunnel"
 nohup cloudflared tunnel run \
   --token "${CF_TUNNEL_TOKEN}" \
-  --url "http://localhost:${PORT}" \
   > /tmp/cloudflared.log 2>&1 &
 # 1.1) Даем 2 секунды на инициализацию
 
